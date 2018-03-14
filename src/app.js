428ea829891723c24454665ed15ac8ac3a8549e8
stormojm <stormojm@gmail.com>
2018-03-14 21:28:28 +0100

    Removing other files from gh-pages branch

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import 'angular-ui-slider';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// extract app name into a config util
-angular.module('swirlApp', ['ui.slider']);
-// Dynamically import place-list so that we are ensured that the module has been created.
-require('./place-list/place-list.component');
--- src/place-tile/place-tile.controller.js
+++
@@ -1,4 +0,0 @@
-export default class PlaceTileController {
-    constructor() {
-    }
-}
